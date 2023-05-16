988885d282fc85366ae4dd45d680fc98bff4231a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-17 00:15:15 +0200

    add home

--- src/pages/visitor/index.tsx
+++ src/pages/visitor/index.tsx
@@ -4,7 +4,7 @@ import { VisitorUrl } from "../../url/VisitorsUrl"
 export const Index=()=>{
     return(
         <>
-            <Navigation items={VisitorUrl}>
+            <Navigation activeUrl="home" items={VisitorUrl} navBarClass={'bg-info text-white'} >
         
             </Navigation>
         </>
