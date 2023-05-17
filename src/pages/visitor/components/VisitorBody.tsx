be2324913386d5b5666a2675d277f8cf554680a6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-18 00:24:41 +0200

    export color

---
+++ src/pages/visitor/components/VisitorBody.tsx
@@ -0,0 +1,7 @@
+export const VisitorBody=()=>{
+    return(
+        <main className="row container-fluid" style={{ background: 'linear-gradient(150deg,#00BFFF,rgb(255, 255, 255))' }}>
+            
+        </main>
+    )
+}
