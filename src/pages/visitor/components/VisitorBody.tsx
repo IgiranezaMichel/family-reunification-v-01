c27c0beb7d7c49f6c9cf6b75c0f570052af096f1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-18 00:27:25 +0200

    import image

--- src/pages/visitor/components/VisitorBody.tsx
+++ src/pages/visitor/components/VisitorBody.tsx
@@ -1,7 +1,14 @@
-export const VisitorBody=()=>{
+import { ReactNode } from "react"
+
+export const VisitorBody=(props:{children:ReactNode})=>{
     return(
         <main className="row container-fluid" style={{ background: 'linear-gradient(150deg,#00BFFF,rgb(255, 255, 255))' }}>
-            
+            <div className="col-md-8">
+                {props.children}
+            </div>
+            <div className="col-sm-4 card">
+                <img src="/public/Visitor/baby-sitting.png" alt="" />
+            </div>
         </main>
     )
 }
