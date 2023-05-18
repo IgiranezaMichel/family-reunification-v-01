5fe6393372b339f3d6d33f87ec42a8c7450bb9ce
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-19 00:33:29 +0200

    styling html

--- src/pages/visitor/components/VisitorBody.tsx
+++ src/pages/visitor/components/VisitorBody.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react"
 
 export const VisitorBody=(props:{children:ReactNode})=>{
     return(
-        <main className="row container-fluid" style={{ background: 'linear-gradient(150deg,#00BFFF,rgb(255, 255, 255))' }}>
-            <div className="col-md-8">
+        <main className="row container-fluid m-auto" style={{ background: 'linear-gradient(150deg,#00BFFF,rgb(255, 255, 255))' }}>
+            <div className="col-md-8 d-flex justify-content-center card bg-transparent border-0">
+                <div>
                 {props.children}
+                </div>
             </div>
-            <div className="col-sm-4 card">
+            <div className="col-sm-4 card p-0 bg-transparent border-0">
                 <img src="/public/Visitor/baby-sitting.png" alt="" />
             </div>
         </main>
