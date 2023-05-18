0e45ba5731f30802c7802bc889e49fa1e212ca91
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-19 00:33:12 +0200

    import Visitor Body

--- src/pages/visitor/index.tsx
+++ src/pages/visitor/index.tsx
@@ -1,11 +1,14 @@
 import { Navigation } from "../../components/Navigation"
 import { VisitorUrl } from "../../url/VisitorsUrl"
+import { VisitorBody } from "./components/VisitorBody"
 
 export const Index=()=>{
     return(
         <>
             <Navigation activeUrl="/" items={VisitorUrl} navBarClass={'bg-info text-white'} >
-        
+                <VisitorBody>
+                    <div className="fw-bold">Content here</div>
+                </VisitorBody>
             </Navigation>
         </>
     )
