4116380e66be6ed865f20f8e9d67ca0d511688fe
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-05-21 13:38:39 +0200

    add content

--- src/pages/visitor/components/VisitorOverBody.tsx
+++ src/pages/visitor/components/VisitorOverBody.tsx
@@ -1,7 +1,9 @@
-export const VisitorOverBody=()=>{
-    return(
-        <main>
-            
+import { ReactNode } from "react"
+
+export const VisitorOverBody = (props: { children: ReactNode }) => {
+    return (
+        <main className="bg-dark text-white" style={{ transform: 'translate(0px,-30px' }}>
+            {props.children}
         </main>
     )
 }
