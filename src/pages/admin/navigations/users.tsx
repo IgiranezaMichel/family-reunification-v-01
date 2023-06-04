a3c97e8b253c122cf01f7ec6fdd2ad167a44c5e4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-06-04 19:36:15 +0200

    add content loading functionality

--- src/pages/admin/navigations/users.tsx
+++ src/pages/admin/navigations/users.tsx
@@ -4,31 +4,44 @@ import { UsersCrud } from "../components/navigationCrud/users"
 import { useUserPage } from "../../../controller/user/queries"
 import { useState } from "react"
 import { PaginationInput } from "../../../typedefs/default/paginationInput"
+import { UserContext } from "../../../context.tsx/UserContext"
 export const Users = () => {
-    const [page,setPage]=useState<PaginationInput>({pageNumber:0,pageSize:10,sort:"firstName"})
-    const {response}=useUserPage(page);
+    const [page, setPage] = useState<PaginationInput>({ pageNumber: 0, pageSize: 10, sort: "firstName" })
+    const { response,refetch } = useUserPage(page);
+    const updateData=()=>{
+        refetch();
+    }
     return (
-        <main className="container-lg">
-            <section className="p-2 bg-primary rounded">
-                <div className="fs-3 fw-bold text-white mb-4">
-                    Manage User
-                </div>
-                <div>
-                    <TextField variant="standard" type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 text-white border-white col-sm-4" />
-                </div>
-                <div className="d-flex">
-                    <Pagination className="text-white" />
-                    <div className="mx-3">
-                        <Sort className="text-white" />
-                        <NativeSelect inputProps={{ name: 'select Gender', id: 'uncontrolled' }}
-                            variant="standard" className="fw-bold border-1 border-white  mb-3">
-                            <option>Sort by</option>
-                            <option>Female</option>
-                        </NativeSelect>
+        <>
+        {!response.responseContent&&<>
+        loading ...
+        </>}
+        {response.responseContent&&<UserContext.Provider value={{data:response.responseContent.content,updateData}}>
+            <main className="container-lg">
+                <section className="p-2 bg-primary rounded">
+                    <div className="fs-3 fw-bold text-white mb-4">
+                        Manage User
                     </div>
-                </div>
-            </section>
-            <UsersCrud />
-        </main>
+                    <div>
+                        <TextField variant="standard" type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 text-white border-white col-sm-4" />
+                    </div>
+                    <div className="d-flex">
+                        <Pagination className="text-white" />
+                        <div className="mx-3">
+                            <Sort className="text-white" />
+                            <NativeSelect onChange={(e)=>setPage({...page,sort:e.target.value})} inputProps={{ name: 'select Gender', id: 'uncontrolled' }}
+                                variant="standard" className="fw-bold text-white border-1 border-white  mb-3">
+                                <option className="text-black" value={'firstName'}>Sort by</option>
+                                <option className="text-black" value={'lastName'}>Female</option>
+                                <option className="text-black" value={'phoneNumber'}>Phone number</option>
+                                <option className="text-black" value={'dob'}>Date of Birth</option>
+                            </NativeSelect>
+                        </div>
+                    </div>
+                </section>
+                <UsersCrud />
+            </main>
+        </UserContext.Provider>}
+        </>
     )
 }
