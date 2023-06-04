86d614ab4988f4539e96b19429258811e1096ddb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-06-04 19:35:31 +0200

    add user detail

--- src/pages/admin/components/navigationCrud/users/detail.tsx
+++ src/pages/admin/components/navigationCrud/users/detail.tsx
@@ -1,26 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Typography } from "@mui/material";
-import { Comment, Description, List, Person, Phone, Wc } from "@material-ui/icons";
+import { Avatar, Divider, Typography } from "@mui/material";
+import { CalendarToday, Comment, Description, Email, List, LocationOn, Map, Person, Phone, Wc } from "@material-ui/icons";
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from "@mui/lab";
 import { BootstrapModal } from "../../../../../components/bootstrapModal";
+import { useUserContext } from "../../../../../context.tsx/UserContext";
+import { useEffect, useState } from "react";
 
-export const UserDetail=(props:{userId:number})=>{
+export const UserDetail=(props:{arrIndex:number})=>{
+    const {data}=useUserContext();
+    const [user,setUser]=useState<any>({});
+    useEffect(
+        ()=>{
+            const fetch=async()=>{
+                if(data!=undefined){
+                    const result=data[props.arrIndex];
+                    setUser(result);
+                }
+            }
+            fetch();
+        }
+    )
 return(
-    <BootstrapModal modalTitle={<div>User Details</div>} id="user-details" size="modal-fullscreen" bg="transparent">
-        <Typography className="row m-auto col-12">
-            <section className="col-sm-3">
-                <div className="card">
-                    <img src="/public/Visitor/baby-sitting.png" className="card-img" />
+    <BootstrapModal modalTitle={
+       <>
+       {user&&<div className="d-flex">
+            <Avatar/>
+            <div className="card justify-content-center border-0"> {user.firstName} {user.lastName} Details</div>
+        </div>}
+       </>
+    } id="user-details" size="modal-fullscreen" bg="transparent">
+        {user&&<Typography className="row m-auto col-12">
+            <section className="col-md-3">
+                <div className="card rounded-0">
+                    <img src={"data:image/png;base64,"+user.profilePicture}className="card-img" />
                 </div>
             </section>
-            <section className="col-sm-9 justify-content-center card border-0">
-                <p><Person/>Mike</p>
-                <p><Wc/>Male</p>
-                <p><Phone/> +250788 2892 90</p>
-                <p><Person/></p>
+            <section className="col-md-4 justify-content-center card border-0">
+                <p><Person/>{user.firstName} {user.lastName}</p>
+                <p><Wc/>{user.gender}</p>
+                <p><Map/>{user.country}</p>
+                <p><Map/>{user.nativeCountry}</p>
+                <p><CalendarToday/>{user.dob}</p>
             </section>
-        </Typography>
-        <div className="overflow-auto">
+            <section className="col-md-4 justify-content-center card border-0">
+                <p><Email/>{user.email} {user.lastName}</p>
+                <p><Phone/>{user.phoneNumber}</p>
+                <p><LocationOn/>{user.address}</p>
+                <p><CalendarToday/>{user.country}</p>
+                <p><CalendarToday/>{user.country}</p>
+            </section>
+        </Typography>}
+        <Divider className="border border-2"/>
+        <div className="overflow-auto container">
         <Timeline sx={{
                 [`& .${timelineItemClasses.root}:before`]: {
                     flex: 0,
