367479fb931e07d67275ebb32c7078d33b19efff
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-20 19:30:34 +0200

    add admin

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import { AdminBody } from "./components/AdminBody"
+import { AdminNavigation } from "./components/AdminNavigation"
+
+export const Admin=()=>{
+    return(
+        <main>
+            <AdminBody>
+                
+            </AdminBody>
+            <AdminNavigation/>
+        </main>
+    )
+}
