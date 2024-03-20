64f360d3e811bec41c20c52ed9f179d35ff5d0d5
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-20 17:59:55 +0200

    styling

--- src/pages/visitor/components/VisitorOverBody.tsx
+++ src/pages/visitor/components/VisitorOverBody.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export const VisitorOverBody = (props: { children: ReactNode }) => {
     return (
-        <main className="text-white" style={{ transform: 'translate(0px,-30px' }}>
+        <main className="text-white col-12 bg-black" style={{ transform: 'translate(0px,-30px' }}>
             {props.children}
         </main>
     )
