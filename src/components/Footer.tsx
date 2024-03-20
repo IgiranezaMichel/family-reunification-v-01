618cd2abdc115dbe6ebed189b7854433069fdfa4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-20 17:58:19 +0200

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer=()=>{
+    return(
+        <main>
+            <div className="fs-5 fw-bold text-center bg-info p-2">
+                All right reserved - designed by the Artist of All the time
+            </div>
+        </main>
+    )
+}
