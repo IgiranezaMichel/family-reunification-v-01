71b294663a7a47d0ae5b70f4564a6708c3ff0f16
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-20 10:29:26 +0200

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Index } from './pages/visitor'
 function App() {
 return (
-    <>
-Abcd
+<>
+      <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Index/>} />
+        </Routes>
+      </div>
+      </Router>
     </>
   )
 }
