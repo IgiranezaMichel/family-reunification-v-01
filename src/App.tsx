3ec732f89dccba406648316eb65432a6ebe9c54a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-20 19:30:23 +0200

    import admin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { Index } from './pages/visitor'
 import { Login } from './pages/visitor/Login'
 import { Signup } from './pages/visitor/Signup'
+import { Admin } from './pages/admin'
 function App() {
 return (
 <>
@@ -11,6 +12,7 @@ return (
           <Route path="/" element={<Index/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/admin" element={<Admin/>} />
         </Routes>
       </div>
       </Router>
