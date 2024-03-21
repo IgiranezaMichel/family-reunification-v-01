b44fbfd04a0d67535d420a87d379e2fc43b09145
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-21 19:50:26 +0200

    remove link

--- src/url/AdminIndexUrl.tsx
+++ src/url/AdminIndexUrl.tsx
@@ -4,16 +4,12 @@ import {UrlType } from "../typedefs/UrlType";
 export const AdminIndexUrl: UrlType[]= [
     {
         title: 'Home',
-        link: '/',
+        link: '/admin',
         icon:<Home/>
 
     },
     {
-        title: 'login',
-        link: '/login',
-    },
-    {
-        title: 'Sign up',
-        link: '/sign-up',
+        title: 'logout',
+        link: '/logout',
     }
 ]
