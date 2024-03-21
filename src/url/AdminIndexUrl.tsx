ad4875b348567de2a1e1f32b1756c016e302df28
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-21 19:43:09 +0200

    add content

---
+++ src/url/AdminIndexUrl.tsx
@@ -0,0 +1,17 @@
+import {UrlType } from "../typedefs/UrlType";
+
+export const AdminIndexUrl: UrlType[]= [
+    {
+        title: 'Home',
+        link: '/',
+
+    },
+    {
+        title: 'login',
+        link: '/login',
+    },
+    {
+        title: 'Sign up',
+        link: '/sign-up',
+    }
+]
