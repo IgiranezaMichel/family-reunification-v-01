ae8b6539b20a15cbf9415be23cd9746aadf415c0
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-21 19:57:06 +0200

    add more about cases

--- src/pages/admin/navigations/cases.tsx
+++ src/pages/admin/navigations/cases.tsx
@@ -1,9 +1,30 @@
+import { Add, Sort } from "@material-ui/icons"
+import { Button, Pagination } from "@mui/material"
+
 export const Cases=()=>{
     return(
-        <>
-        <div className="display-6 fw-bold">
+        <div>
+        <div className="fs-4 fw-bold">
             Manage Cases
         </div>
-        </>
+        <div>
+            <input type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 border-dark col-sm-4"/>
+        </div>
+        <div className="d-flex">
+            <Pagination/> 
+            <div className="mx-3">
+            <Sort/><select className="rounded-2 px-1">
+                <option value="">sort by</option>
+                <option value="">Name</option>
+                <option value="">sort by</option>
+            </select>
+            </div>
+        </div>
+        <div className="mt-3">
+            <Button variant="contained" className="">
+                <Add/> New
+            </Button>
+        </div>
+        </div>
     )
 }
