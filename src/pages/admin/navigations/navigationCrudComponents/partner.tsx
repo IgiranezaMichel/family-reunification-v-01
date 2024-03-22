4bf67e031680a4c5e946d9f842b2ab39de251af4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-22 14:19:56 +0200

    create parter crud

---
+++ src/pages/admin/navigations/navigationCrudComponents/partner.tsx
@@ -0,0 +1,23 @@
+export const PartnerCrud=(props:{search:string,page:PaginationInput})=>{
+    const updateHandler=()=>{
+
+    }
+    const deleteHandler=()=>{
+        
+    }
+    const updateView=<>
+    </>
+       const deleteView=<>
+       </>
+    const displayView=<>
+    {updateView}
+    {deleteView}
+    </>
+
+   
+    return(
+        <>
+        {displayView}
+        </>
+    )
+}
