3a1b0409a594b6c74e651753bd4da55f48b9c75b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-22 16:52:17 +0200

    rename

--- src/pages/admin/components/Modal.tsx
+++ src/pages/admin/components/Modal.tsx
@@ -1,14 +1,14 @@
 import { Box,Modal } from "@mui/material";
 import { FC, ReactNode } from "react";
 type ModalItem={
-    width:'540px'|'720px'|'960px'|'1140px',
+    width:'30%'|'540px'|'720px'|'960px'|'1140px',
     bg:string,
     openModal:boolean,
     children:ReactNode
 }
 export const CardModal:FC<ModalItem>=(props)=>{
     const style = {
-        position: 'absolute' as 'absolute',
+        position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
@@ -23,11 +23,11 @@ export const CardModal:FC<ModalItem>=(props)=>{
       <Modal
         keepMounted
         open={props.openModal}
-        style={{backgroundColor:'transparent'}}
+        style={{backgroundColor:'transparent',overflow:'auto'}}
         aria-labelledby="keep-mounted-modal-title"
         aria-describedby="keep-mounted-modal-description"
       >
-        <Box sx={style}>
+        <Box sx={style} >
             {props.children}
         </Box>
       </Modal>
