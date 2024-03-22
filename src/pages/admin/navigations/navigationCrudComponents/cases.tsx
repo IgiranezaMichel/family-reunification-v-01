f9182893578177b9a68edb0e7097957739d7d5bb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-22 14:19:29 +0200

    create cases crud

---
+++ src/pages/admin/navigations/navigationCrudComponents/cases.tsx
@@ -0,0 +1,22 @@
+import { PaginationInput } from "../../../../typedefs/default/paginationInput"
+
+export const CasesCrud=(props:{search:string,page:PaginationInput})=>{
+    const updateHandler=()=>{
+
+    }
+    const deleteHandler=()=>{
+        
+    }
+    const displayView=<>
+    
+    </>
+
+    const updateView=<>
+    </>
+       const deleteView=<>
+       </>
+    return(
+        <>
+        </>
+    )
+}
