34073b6435466b98d75795a85a52d9d6bf80afce
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-22 09:00:36 +0200

    add props

--- src/pages/admin/components/AdminBody.tsx
+++ src/pages/admin/components/AdminBody.tsx
@@ -1,27 +1,38 @@
-import { Home } from "@material-ui/icons"
+import { FileCopyRounded, Home, Message, People, ReportRounded, SettingsApplications } from "@material-ui/icons"
 import { Card } from "@mui/material"
 
-export const AdminBody=()=>{
+export const AdminBody=(props:{active:string})=>{
     return(
         <>
-        <div className="row m-auto col-sm-9 d-flex justify-content-center align-items-center">
+        <div className="row m-auto col-sm-9 d-flex m-auto justify-content-center align-items-center">
             <div className="col-sm-1">
-            <Card className="bg-info d-flex justify-content-center align-items-center" style={{rotate:'45deg',width:'50px',height:'50px'}} >
+            <Card className="bg-info d-flex justify-content-center align-items-center" style={{rotate:'45deg',width:'100px',height:'100px'}} >
                 <div style={{rotate:'-45deg'}}>
-                <Home className="fs-1" />
+                {props.active=='home'?<Home className="fs-1" />
+                :props.active=='partner'?<People className="fs-1"/>
+                :props.active=='users'?<People className="fs-1"/>
+                :props.active=='cases'?<FileCopyRounded className="fs-1"/>
+                :props.active=='report'?<ReportRounded className="fs-1"/>
+                :props.active=='chat'?<Message className="fs-1"/>
+                :props.active=='settings'?<SettingsApplications className="fs-1"/>:
+                ''}
                 </div>
             </Card>
             </div>
-            <Card className="col-sm-10">
-                <div className="display-6 fw-bold">
+            
+            <div className="card border-0 col-sm-10" style={{zIndex:-1}}>
+                <img src="/Visitor/mayperson.png" className="card-img"/>
+               <div className="card-img-overlay p-5">
+               <div className="display-6 fw-bold text-white">
                     Family Reunification and tracking
                 </div>
-                <div className="mt-4 fs-5 fw-bold">
-                    /home
+                <div className="mt-5 text-white fs-5 fw-bold">
+                    /{props.active}
                 </div>
-            </Card>
+               </div>
+            </div>
             <div className="col-sm-1">
-            <Card className="bg-info d-flex justify-content-center align-items-center" style={{rotate:'45deg',width:'50px',height:'50px'}} >
+            <Card className="bg-info d-flex justify-content-center align-items-center" style={{rotate:'45deg',width:'100px',height:'100px'}} >
                 <div style={{rotate:'-45deg'}} className="p-1">
                 <img src="Visitor/baby-sitting.png" className="card-img"/>
                 </div>
