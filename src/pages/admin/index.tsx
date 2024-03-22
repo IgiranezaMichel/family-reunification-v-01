405291d3949ad33ba883215c2611ea7673f3d2fb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-22 09:00:27 +0200

    customize body

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,14 +1,10 @@
 import { Navigation } from "../../components/Navigation"
 import { AdminIndexUrl } from "../../url/AdminIndexUrl"
-import { AdminBody } from "./components/AdminBody"
 import { AdminNavigation } from "./components/AdminNavigation"
 
 export const Admin=()=>{
     return(
         <Navigation activeUrl="/admin" items={AdminIndexUrl} navBarClass="bg-danger nav text-white">
-            <AdminBody>
-
-            </AdminBody>
             <AdminNavigation/>
         </Navigation>
     )
