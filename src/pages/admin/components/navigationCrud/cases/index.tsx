93e4b9dd849efde28d82ef8b5c5cbaa1a060b09b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-24 21:09:34 +0200

    add ui on case|Crud

--- src/pages/admin/components/navigationCrud/cases/index.tsx
+++ src/pages/admin/components/navigationCrud/cases/index.tsx
@@ -1,22 +1,58 @@
+import { Button, Table, TableCell, TableRow, Typography } from "@mui/material"
 import { PaginationInput } from "../../../../../typedefs/default/paginationInput"
+import { Add, Delete, Details, List, Update } from "@material-ui/icons"
+import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from "@mui/lab"
 
-export const CasesCrud=(props:{search:string,page:PaginationInput})=>{
-    const updateHandler=()=>{
+export const CasesCrud = (props: { search: string, page: PaginationInput }) => {
+    const updateHandler = () => {
 
     }
-    const deleteHandler=()=>{
-        
+    const deleteHandler = () => {
+
     }
-    const displayView=<>
-    
+    const displayView = <>
+
     </>
 
-    const updateView=<>
+    const updateView = <>
     </>
-       const deleteView=<>
-       </>
-    return(
+    const deleteView = <>
+    </>
+    return (
         <>
+
+            <div className="mt-3">
+                <Button variant="contained" className="">
+                    <Add /> New
+                </Button>
+            </div>
+            <Timeline sx={{
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                },
+            }}>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            Eat
+                        </Typography>
+                        <Typography>Because you need strength</Typography>
+                        <Typography>
+                            <small>on 23/07/2023</small> 
+                            <div className="modal-footer">
+                                <Update className="bg-success text-white p-1 fs-3 rounded-circle m-1"/> 
+                                <Delete className="bg-danger text-white p-1 fs-3 rounded-circle m-1"/> 
+                                <List className="bg-info p-1 fs-3 rounded-circle m-1"/>
+                            </div>
+                        </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>
         </>
     )
 }
