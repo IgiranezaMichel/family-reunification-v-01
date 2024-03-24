a071f77b11c311b1d2dd019243f4a228cda66953
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-24 14:34:29 +0200

    change container to contained

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -6,7 +6,7 @@ export const Logout = () => {
             <div className="bg-primary p-2 rounded">
                 <p className="text-white">Are you sure you want to Logout?</p>
                 <div className="modal-footer">
-                    <Button className="text-white border border-2 fw-bold" variant="container">
+                    <Button className="text-white border border-2 fw-bold" variant="contained">
                         Yes
                     </Button>
                 </div>
