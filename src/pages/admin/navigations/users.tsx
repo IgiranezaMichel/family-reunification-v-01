e92e40a6d53eea48e45fdfcb6970a6a94e8a08b2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-24 11:57:21 +0200

    styling

--- src/pages/admin/navigations/users.tsx
+++ src/pages/admin/navigations/users.tsx
@@ -1,25 +1,29 @@
 import { Sort } from "@material-ui/icons"
 import { Pagination } from "@mui/material"
+import { UsersCrud } from "./navigationCrudComponents/users"
 
 export const Users=()=>{
     return(
-        <>
-        <div className="fs-3 fw-bold">
+        <main className="container-lg">
+        <section className="p-2 bg-primary rounded">
+        <div className="fs-3 fw-bold text-white mb-4">
             Manage User
         </div>
         <div>
-            <input type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 border-dark col-sm-4"/>
+            <input type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 bg-primary text-white border-white col-sm-4"/>
         </div>
         <div className="d-flex">
-            <Pagination/> 
+            <Pagination className="text-white"/> 
             <div className="mx-3">
-            <Sort/><select className="rounded-2 px-1">
+            <Sort className="text-white"/><select className="rounded-2 px-1 bg-primary text-white border-white">
                 <option value="">sort by</option>
                 <option value="">Name</option>
                 <option value="">sort by</option>
             </select>
             </div>
         </div>
-        </>
+        </section>
+        <UsersCrud/>
+        </main>
     )
 }
