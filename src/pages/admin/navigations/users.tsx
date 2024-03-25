d6b70f382e7047c01cfe50afb36aabd40f355b21
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-25 09:25:02 +0200

    add text field

--- src/pages/admin/navigations/users.tsx
+++ src/pages/admin/navigations/users.tsx
@@ -1,29 +1,29 @@
 import { Sort } from "@material-ui/icons"
-import { Pagination } from "@mui/material"
+import { NativeSelect, Pagination, TextField } from "@mui/material"
 import { UsersCrud } from "../components/navigationCrud/users"
-
-export const Users=()=>{
-    return(
+export const Users = () => {
+    return (
         <main className="container-lg">
-        <section className="p-2 bg-primary rounded">
-        <div className="fs-3 fw-bold text-white mb-4">
-            Manage User
-        </div>
-        <div>
-            <input type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 bg-primary text-white border-white col-sm-4"/>
-        </div>
-        <div className="d-flex">
-            <Pagination className="text-white"/> 
-            <div className="mx-3">
-            <Sort className="text-white"/><select className="rounded-2 px-1 bg-primary text-white border-white">
-                <option value="">sort by</option>
-                <option value="">Name</option>
-                <option value="">sort by</option>
-            </select>
-            </div>
-        </div>
-        </section>
-        <UsersCrud/>
+            <section className="p-2 bg-primary rounded">
+                <div className="fs-3 fw-bold text-white mb-4">
+                    Manage User
+                </div>
+                <div>
+                    <TextField variant="standard" type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 text-white border-white col-sm-4" />
+                </div>
+                <div className="d-flex">
+                    <Pagination className="text-white" />
+                    <div className="mx-3">
+                        <Sort className="text-white" />
+                        <NativeSelect inputProps={{ name: 'select Gender', id: 'uncontrolled' }}
+                            variant="standard" className="fw-bold border-1 border-white  mb-3">
+                            <option>Sort by</option>
+                            <option>Female</option>
+                        </NativeSelect>
+                    </div>
+                </div>
+            </section>
+            <UsersCrud />
         </main>
     )
 }
