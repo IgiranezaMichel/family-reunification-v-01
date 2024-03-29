f3eeabdfef3f864c9421872388b9b9fdba047411
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-29 11:05:39 +0200

    add uplifting

---
+++ src/context.tsx/demo/context.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext } from "react";
+import { UpliftingContextType } from "./type";
+
+export const UpliftingContext = createContext<UpliftingContextType | undefined>(undefined);
+export const useUpliftingMessage = (): UpliftingContextType => {
+    const context = useContext(UpliftingContext);
+    if (!context) {
+      throw new Error('useUpliftingMessage must be used within a UpliftingProvider');
+    }
+    return context;
+  };
