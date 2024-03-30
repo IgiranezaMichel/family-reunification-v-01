8ada9a37b776ac680633f3f3bccd45a7d5dbad1e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-30 20:16:11 +0200

    add useOrganization query

--- src/controller/organization/query.tsx
+++ src/controller/organization/query.tsx
@@ -1,10 +1,20 @@
 import { useQuery } from "@apollo/client";
 import { PaginationInput } from "../../typedefs/default/paginationInput";
 import { ORGANIZATION_PAGE } from "../../graphql/query/organization";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Response } from "../../typedefs/response";
 
 export const useOrganization=(organizationInput:PaginationInput)=>{
 const [response,setResponse]=useState<Response>({code:200,responseContent:[],responseReady:false});
-const page=useQuery(ORGANIZATION_PAGE,{variables:{organizationInput:organizationInput}});
+const {refetch,data}=useQuery(ORGANIZATION_PAGE,{variables:{organizationInput:organizationInput}});
+useEffect(
+    ()=>{
+        const fetch=async()=>{
+            if(data.organizationPage) return data.organizationPage;
+        }
+        fetch().then(data=>setResponse({code:200,responseContent:data,responseReady:true}))
+        .catch(err=>console.log(err));
+    }
+)
+return {refetch,response}
 }
