000878dd3e05f0a5d152f31c56a0df311acc8084
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-30 22:46:12 +0200

    add refetch data

--- src/controller/organization/query.tsx
+++ src/controller/organization/query.tsx
@@ -6,15 +6,15 @@ import { Response } from "../../typedefs/response";
 
 export const useOrganization=(organizationInput:PaginationInput)=>{
 const [response,setResponse]=useState<Response>({code:200,responseContent:[],responseReady:false});
-const {refetch,data}=useQuery(ORGANIZATION_PAGE,{variables:{organizationInput:organizationInput}});
+const {refetch,data}=useQuery(ORGANIZATION_PAGE,{variables:{organizationPageInput:organizationInput}});
 useEffect(
     ()=>{
         const fetch=async()=>{
-            if(data.organizationPage) return data.organizationPage;
+            if(data) return data.organizationPage;
         }
         fetch().then(data=>setResponse({code:200,responseContent:data,responseReady:true}))
         .catch(err=>console.log(err));
-    }
+    },[data, organizationInput]
 )
 return {refetch,response}
 }
