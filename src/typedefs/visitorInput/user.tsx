87b882640e1beea4308d40cfff3a813a326d8c88
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-01 11:02:06 +0200

    add queries

--- src/typedefs/visitorInput/user.tsx
+++ src/typedefs/visitorInput/user.tsx
@@ -1,17 +1,18 @@
 import { Gender } from "../../enum/gender"
 
 export type UserInput={
-    id?:number,
-    firstName:string,
-    lastName:string,
-    gender:Gender,
-    email:string,
-    dob:string,
-    phoneNumber:string,
-    address?:string,
-    country:string,
-    nativeCountry:string,
-    userName:string,
-    password:string,
+    id?:number
+    firstName:string
+    lastName:string
     profilePicture:string
+    gender:Gender
+    email:string
+    phoneNumber:string
+    dob:string
+    address:string
+    country:string
+    nativeCountry:string
+    role?:string
+    username:string
+    password:string
 }
