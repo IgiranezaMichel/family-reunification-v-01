13854781ee09b8d3529b237ebdfa1dfa57389431
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 12:56:37 +0200

    add CaseCrud

--- src/pages/admin/components/navigationCrud/cases/index.tsx
+++ src/pages/admin/components/navigationCrud/cases/index.tsx
@@ -1,45 +1,31 @@
 import { Button, Typography } from "@mui/material"
-import { PaginationInput } from "../../../../../typedefs/default/paginationInput"
-import { Add, Delete, List, Update } from "@material-ui/icons"
-import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from "@mui/lab"
+import { Add, Delete, List,UpdateOutlined } from "@material-ui/icons"
+import { CreateCase } from "./create"
+import { useState } from "react"
+import { TimelineComponent } from "../../../../../components/TimelineComponent"
 
-export const CasesCrud = (props: { search: string, page: PaginationInput }) => {
-
+export const CasesCrud = (props: { search: string}) => {
+    const [action,setAction]=useState('Add Case');
+    console.log(props.search)
     return (
         <>
-
             <div className="mt-3">
-                <Button variant="contained" className="">
+                <Button variant="contained"
+                data-bs-toggle="modal" data-bs-target='#addCase' className="" onClick={()=>setAction('Create Case')}>
                     <Add /> New
                 </Button>
             </div>
-            <Timeline sx={{
-                [`& .${timelineItemClasses.root}:before`]: {
-                    flex: 0,
-                    padding: 0,
-                },
-            }}>
-                <TimelineItem>
-                    <TimelineSeparator>
-                        <TimelineDot />
-                        <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ py: '12px', px: 2 }}>
-                        <Typography variant="h6" component="span">
-                            Eat
-                        </Typography>
-                        <Typography>Because you need strength</Typography>
-                        <Typography>
+             <TimelineComponent title="Title">
+             <Typography>
                             <small>on 23/07/2023</small> 
                             <div className="modal-footer">
-                                <Update className="bg-success text-white p-1 fs-3 rounded-circle m-1"/> 
+                                <UpdateOutlined className="bg-success text-white p-1 fs-3 rounded-circle m-1"/> 
                                 <Delete className="bg-danger text-white p-1 fs-3 rounded-circle m-1"/> 
                                 <List className="bg-info p-1 fs-3 rounded-circle m-1"/>
                             </div>
                         </Typography>
-                    </TimelineContent>
-                </TimelineItem>
-            </Timeline>
+             </TimelineComponent>
+            <CreateCase action={action}/>
         </>
     )
 }
