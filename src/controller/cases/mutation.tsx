2bad5d44c042aa09ae582c32ccb68b50e84b85b5
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 20:30:48 +0200

    create useSaveCase hook

---
+++ src/controller/cases/mutation.tsx
@@ -0,0 +1,34 @@
+import { useMutation } from "@apollo/client"
+import { CaseInput } from "../../typedefs/cases"
+import { DELETE_CASE, REGISTER_CASE } from "../../graphql/mutation/cases"
+import { useState } from "react";
+import { Response } from "../../typedefs/response";
+
+export const useSaveCases=(caseInput:CaseInput,customerId:number)=>{
+    const [save]=useMutation(REGISTER_CASE);
+    const [response,setResponse]=useState<Response>({
+        code:0,responseContent:'',responseReady:false
+    })
+    const saveHandler=async()=>{
+        save({variables:{caseInput:caseInput,customerId:customerId}})
+        .then((data)=>{
+        const result=data.data.saveCases.split[''];
+        setResponse({code:Number(result[0]),responseContent:result[1],responseReady:true});
+        })
+    }
+    return {saveHandler,response}
+}
+export const useDeleteCases=(caseId:number)=>{
+    const [deleteCase]=useMutation(DELETE_CASE);
+    const [response,setResponse]=useState<Response>({
+        code:0,responseContent:'',responseReady:false
+    })
+    const deleteHandler=async()=>{
+        deleteCase({variables:{caseId:caseId}})
+        .then((data)=>{
+        const result=data.data.deleteCases.split[''];
+        setResponse({code:Number(result[0]),responseContent:result[1],responseReady:true});
+        })
+    }
+    return {deleteHandler,response}
+}
