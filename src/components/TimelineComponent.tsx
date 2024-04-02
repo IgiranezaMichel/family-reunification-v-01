8fbed7749ca857645cbc3c2f4c00c387eba31e80
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 12:52:23 +0200

    create timeline Component

---
+++ src/components/TimelineComponent.tsx
@@ -0,0 +1,36 @@
+import { Comment, Description, List } from "@material-ui/icons"
+import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from "@mui/lab"
+import { Typography } from "@mui/material"
+import { FC, ReactNode } from "react"
+type TimelineItem={
+    title:string,
+    children:ReactNode
+}
+export const TimelineComponent:FC<TimelineItem> = (props) => {
+    return (
+        <div>
+            <Timeline sx={{
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                },
+            }}>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h6" component="span">
+                            {props.title}
+                        </Typography>
+                        <Typography>
+                            {props.children}
+                        </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>
+            
+        </div>
+    )
+}
