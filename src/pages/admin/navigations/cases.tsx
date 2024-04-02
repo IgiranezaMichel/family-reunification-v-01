2cbb028566dcc6eb88f059c41a22c7e8a8f887fe
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 17:42:49 +0200

    add native select

--- src/pages/admin/navigations/cases.tsx
+++ src/pages/admin/navigations/cases.tsx
@@ -1,5 +1,5 @@
 import { Sort } from "@material-ui/icons"
-import { Pagination } from "@mui/material"
+import { NativeSelect, Pagination, TextField } from "@mui/material"
 import { CasesCrud } from "../components/navigationCrud/cases"
 
 export const Cases=()=>{
@@ -9,21 +9,24 @@ export const Cases=()=>{
      <div className="fs-4 fw-bold">
             Manage Cases
         </div>
+        <div className="mb-4 fw-bold text-white">
+            Total Cases
+        </div>
         <div>
-            <input type="search" placeholder="search ..." className="float-end border-0 border-bottom border-2 border-dark col-sm-4"/>
+            <TextField type="search" placeholder="search ..." variant="standard" className="float-end text-white border-0 border-bottom border-2 border-dark col-sm-4" style={{color:'white !important'}}/>
         </div>
         <div className="d-flex">
             <Pagination/> 
             <div className="mx-3">
-            <Sort/><select className="rounded-2 px-1">
+            <Sort/><NativeSelect className="rounded-2 px-1">
                 <option value="">sort by</option>
                 <option value="">Name</option>
                 <option value="">sort by</option>
-            </select>
+            </NativeSelect>
             </div>
         </div>
      </section>
-        <CasesCrud />
+        <CasesCrud search=""/>
         </main>
     )
 }
