e08b31e5af6040e836793e084dd283c507d4b42b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 21:36:23 +0200

    redefine create case

--- src/pages/admin/components/navigationCrud/cases/create.tsx
+++ src/pages/admin/components/navigationCrud/cases/create.tsx
@@ -1,20 +1,43 @@
 import { Button, TextField, TextareaAutosize } from "@mui/material"
 import { BootstrapModal } from "../../../../../components/bootstrapModal"
 import { Save } from "@material-ui/icons"
+import { useSaveCases } from "../../../../../controller/cases/mutation"
+import { useEffect, useState } from "react"
+import { CaseInput } from "../../../../../typedefs/cases"
+import { Toast } from "../../../../../components/toast"
+import { snack } from "../../../../../typedefs/snack"
 
 export const CreateCase=(props:{action:string})=>{
+    const [cases,setCases]=useState<CaseInput>({description:'',title:'',id:0});
+    const customerId=1;
+    const [snack,setSnack]=useState<snack>({message:'',open:false});
+    const [open,setOpen]=useState(false);
+    const saveCase=useSaveCases(cases,customerId);
+    useEffect(
+        ()=>{
+            const fetch=async()=>{
+                return saveCase;    
+            }
+            fetch();
+        },[saveCase,snack]
+    )
+    const saveCaseHandler=()=>{
+        saveCase.saveHandler()
+        .then(()=>alert(saveCase.response.responseContent));
+    }
     return <>
     <BootstrapModal size="modal-md"  id="addCase"
     modalTitle={
         <div>{props.action}</div>
     }>
         <div>
-            <TextField className="col-12" variant="standard" label='Case Name'/>
-            <TextareaAutosize className="col-12 mt-3" minRows={6} placeholder='Case Description'/>
+            <TextField value={cases.title} onChange={(e)=>setCases({...cases,title:e.target.value})} className="col-12" variant="standard" label='Case Name'/>
+            <TextareaAutosize value={cases.description} onChange={(e)=>setCases({...cases,description:e.target.value})} className="col-12 mt-3" minRows={6} placeholder='Case Description'/>
             <div className="modal-footer">
-                <Button variant="outlined"><Save/></Button>
+                <Button variant="outlined" onClick={()=>saveCaseHandler()}><Save/></Button>
             </div>
         </div>
     </BootstrapModal>
+    <Toast horizontal="left" open={open} vertical="bottom" message={snack.message}/>
     </>
 }
