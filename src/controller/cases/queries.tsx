c5218150ac7b88bd1b76185572df0a04dc8b588f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-04 18:20:53 +0200

    add useCaseList

---
+++ src/controller/cases/queries.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@apollo/client"
+import { LIST_OF_CASE } from "../../graphql/query/cases"
+import { useEffect, useState } from "react";
+
+export const useCaseList = () => {
+  const result = useQuery(LIST_OF_CASE);
+  const [response, setResponse] = useState({ code: 0, responseContent: [], responseReady: false });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (result.data) {
+          const caseList = await result.data.caseList;
+          setResponse({ ...response, responseContent: caseList, responseReady: true });
+        }
+      } catch (error) {
+        console.error('Error fetching case list:', error);
+      }
+    };
+
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [result]);
+
+  return { response, result };
+};
