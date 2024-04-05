c73e28c77f884a8a96dea416040d1bbd7154b733
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-05 16:58:10 +0200

    refactoring

---
+++ src/pages/customer/navigationCrud/Claims/claims.tsx
@@ -0,0 +1,12 @@
+import ClaimTabs from "./claimsTab"
+
+export const ClaimCrud = () => {
+    return (
+        <>
+            <div className="overflow-auto">
+            
+            <ClaimTabs/>
+            </div>
+        </>
+    )
+}
