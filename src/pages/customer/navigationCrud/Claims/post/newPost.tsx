f1959ba1644ac8d61b89c26e9ee2f07c5ff4863f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-06 19:13:30 +0200

    add file handler

--- src/pages/customer/navigationCrud/Claims/post/newPost.tsx
+++ src/pages/customer/navigationCrud/Claims/post/newPost.tsx
@@ -1,4 +1,5 @@
-import { FormControl, NativeSelect, TextField } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FormControl, InputLabel, NativeSelect, TextField } from "@mui/material";
 import { BootstrapModal } from "../../../../../components/bootstrapModal";
 import JoditEditor from "jodit-react";
 import { useState } from "react";
@@ -8,6 +9,32 @@ import { ClaimUserHasNoAccount } from "./userHasNoAccount";
 export default function NewPost() {
     const [lostPerson, setLostPerson] = useState<LostPersonHavingAccount>({ case: '', description: '', email: '' });
     const [hasAccount,setHasAccount]=useState('');
+    const handleFileChange = (event:any) => {
+        const fileList = event.target.files;
+        const filesArray = Array.from(fileList);
+        // Read files as base64 and update state
+        Promise.all(filesArray.map(file => readFileData(file)))
+          .then(base64Array => console.log(base64Array));
+      };
+    
+  // Function to read file data
+  const readFileData = (file:any) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      // Extract file name and extension
+      const fileName = file.name;
+      const fileExtension = fileName.split('.').pop();
+
+      reader.onload = () => {
+        const base64Data = reader.result;
+        resolve({ fileName, fileExtension, base64Data });
+      };
+
+      reader.onerror = (error) => reject(error);
+    });
+  };
     const userHasAccount = <>
         <TextField className="col-12 mb-3" value={lostPerson.email} onChange={(e) => setLostPerson({ ...lostPerson, email: e.target.value })} variant="standard" label='Enter lost Person Email Address' />
         <FormControl fullWidth className="mb-3">
@@ -15,6 +42,11 @@ export default function NewPost() {
                 <option value="">Select case</option>
             </NativeSelect>
         </FormControl>
+        <FormControl fullWidth>
+            <label>Select documents/photos</label>
+            <input type="file" onChange={handleFileChange} multiple/>
+        </FormControl>
+        <div>Add Description</div>
         <JoditEditor value={lostPerson.description} onChange={(e) => setLostPerson({ ...lostPerson, description: e })}>
         </JoditEditor>
     </>
