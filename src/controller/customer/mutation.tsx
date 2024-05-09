9dd189607eb88c0574d87741cb9a65a1a78fcb70
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-09 15:21:25 +0200

    refactor delete handler

--- src/controller/customer/mutation.tsx
+++ src/controller/customer/mutation.tsx
@@ -1,7 +1,5 @@
 import { useMutation } from "@apollo/client";
-import { useState } from "react";
 import {DELETE_CUSTOMER, REGISTER_CUSTOMER } from "../../graphql/mutation/customer";
-import { Response } from "../../typedefs/response";
 import { CustomerInput } from "../../typedefs/visitorInput/customer";
 
 export const useSaveCustomer=(customerInput:CustomerInput)=>{
@@ -13,14 +11,8 @@ return {saveHandler}
 }
 export const useDeleteCustomer=(id:number)=>{
     const [deleteCustomer]=useMutation(DELETE_CUSTOMER);
-    const [response,setResponse]=useState<Response>({
-        code:0,responseContent:'',responseReady:false
-    })
-    const deleteHandler=()=>{
-        deleteCustomer({variables:{id:id}}).then(data=>{
-            const result=data.data.saveCustomer.split[''];
-            setResponse({code:Number(result[0]),responseContent:result[1],responseReady:true});
-        }).catch(err=>console.log(err))
+    const deleteHandler=async()=>{
+       return await deleteCustomer({variables:{id:id}});
     }
-    return {deleteHandler,response}
+    return {deleteHandler}
     }
