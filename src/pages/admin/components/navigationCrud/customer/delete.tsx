83cd9fb8dbcb4bc6b43f7c14b07898ff7299358b
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-09 15:21:47 +0200

    add use delete customer

--- src/pages/admin/components/navigationCrud/customer/delete.tsx
+++ src/pages/admin/components/navigationCrud/customer/delete.tsx
@@ -4,10 +4,12 @@ import { Button } from "@mui/material"
 import { useEffect, useState } from "react"
 import { BootstrapModal } from "../../../../../components/bootstrapModal"
 import { useCustomerContext } from "../../../../../context.tsx/customerContext"
+import { useDeleteCustomer } from "../../../../../controller/customer/mutation"
 
 export const DeleteUser=(props:{arrIndex:number})=>{
-    const {data}=useCustomerContext();
+    const {data,updateData}=useCustomerContext();
     const [customer,setCustomer]=useState<any>({});
+    const {deleteHandler}=useDeleteCustomer(customer.id)
     useEffect(
         ()=>{
             const fetch=async()=>{
@@ -19,11 +21,17 @@ export const DeleteUser=(props:{arrIndex:number})=>{
             fetch();
         }
     )
+    const deleteCustomer=()=>{
+        deleteHandler()
+        .then(data=>{
+            console.log(data);updateData()
+        }).catch()
+    }
     return <>
     <BootstrapModal id="delete" size="modal-sm" >
         {customer&&<div>Are you sure you want to remove <b>{customer.firstName} {customer.lastName}</b>?</div>}
         <div className="modal-footer">
-            <Button><Delete /></Button>
+            <Button onClick={()=>deleteCustomer()}><Delete /></Button>
         </div>
     </BootstrapModal>
     </>
