d49e69c5dda5fd2ab441580784542ba5785d3725
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-15 17:33:20 +0200

    add userDob

--- src/pages/admin/components/navigationCrud/customer/index.tsx
+++ src/pages/admin/components/navigationCrud/customer/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CalendarToday, Delete, Email, LocationOn, PersonAdd, Phone, PostAddOutlined, Update, Wc } from "@material-ui/icons"
-import { Avatar, Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
+import { CalendarTodaySharp, Delete, Email, LocationOn, PersonAdd, Phone, PostAddOutlined, Update, Wc } from "@material-ui/icons"
+import { Avatar, Button, Card, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
 import { useState } from "react"
 import { useCustomerContext } from "../../../../../context.tsx/customerContext"
 import { AddUser } from "./createOrUpdate"
@@ -17,21 +17,21 @@ export const CustomerCrud = () => {
             (result: any, index: number) => {
                 return <TableRow key={index} className="border border-4 border-white">
                     <TableCell className="col-2 rounded-0">
-                       <section className="d-flex">
-                       <Avatar src={'data:image/png;base64,' + result.profilePicture} />
-                        <div className="text-center card d-flex justify-content-center bg-transparent border-0">
-                            <b className="m-auto">{result.firstName} {result.lastName}</b>
-                        </div>
-                       </section>
+                        <section className="d-flex">
+                            <Avatar src={'data:image/png;base64,' + result.profilePicture} />
+                            <div className="text-center card d-flex justify-content-center bg-transparent border-0">
+                                <b className="m-auto">{result.firstName} {result.lastName}</b>
+                            </div>
+                        </section>
+                        <p className="mt-1"><Wc /> {result.gender}</p>
                     </TableCell>
                     <TableCell>
-                        <p><Phone />{result.phoneNumber}</p>
-                        <p><Email />{result.email}</p>
+                        <p><Phone className="rounded-circle p-1 mx-1 border border-dark" />{result.phoneNumber}</p>
+                        <p><Email className="rounded-circle p-1 mx-1 border border-dark" />{result.email}</p>
                     </TableCell>
                     <TableCell>
-                        <p><Wc /> {result.gender}</p>
-                        <p><LocationOn /> {result.address}</p>
-                        <p><CalendarToday /> {result.dob} </p>
+                        <p><LocationOn className="rounded-circle p-1 mx-1 border border-dark" /> {result.address}</p>
+                        <p><CalendarTodaySharp className="rounded-circle p-1 mx-1 border border-dark" /> {result.userDob} </p>
                     </TableCell>
                     <TableCell className="text-center">
                         <PostAddOutlined data-bs-toggle="modal" onClick={() => setArrIndex(index)}
@@ -57,17 +57,24 @@ export const CustomerCrud = () => {
         <>
             <Button className="mt-3" data-bs-toggle="modal"
                 data-bs-target="#add-newUser" onClick={() => setAction('addUser')} variant="contained"><PersonAdd /></Button>
-            <Table className="border mt-3 mb-5 table-responsive">
-                <TableHead>
-                    <TableRow className="bg-body-secondary ">
-                        <TableCell className="fw-bold">Name</TableCell>
-                        <TableCell className="fw-bold">Contact detail</TableCell>
-                        <TableCell className="fw-bold">Others</TableCell>
-                        <TableCell className="fw-bold text-center">Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                {display}
-            </Table>
+            <Card elevation={4} className="p-0 mt-2">
+                <Table className="border table-responsive">
+                    <TableHead>
+                        <TableRow className="bg-body-secondary ">
+                            <TableCell className="fw-bold">Profile</TableCell>
+                            <TableCell className="fw-bold">Contacts</TableCell>
+                            <TableCell className="fw-bold">Others</TableCell>
+                            <TableCell className="fw-bold text-center">Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {display}
+                </Table>
+                {data != undefined && data.length != 0 && <div className="modal-footer">
+                    {data.pageNumber} page out of {data.pageSize}
+
+                    size
+                </div>}
+            </Card>
             <DeleteUser arrIndex={arrIndex} />
             <CustomerDetail arrIndex={arrIndex} />
             <AddUser action={action} arrIndex={arrIndex} />
