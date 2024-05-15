5a85b13f5a6b9109701e4ad749855ae8c3fd1310
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-15 17:31:49 +0200

    refactor formatting document

--- src/controller/customer/mutation.tsx
+++ src/controller/customer/mutation.tsx
@@ -6,16 +6,10 @@ import { CustomerInput } from "../../typedefs/visitorInput/customer";
 
 export const useSaveCustomer=(customerInput:CustomerInput)=>{
 const [saveCustomer]=useMutation(REGISTER_CUSTOMER);
-const [response,setResponse]=useState<Response>({
-    code:0,responseContent:'',responseReady:false
-})
 const saveHandler=async()=>{
-    await saveCustomer({variables:{customerInput:customerInput}}).then(data=>{
-        const result=data.data.saveCustomer.split[''];
-        setResponse({code:Number(result[0]),responseContent:result[1],responseReady:true});
-    }).catch(err=>console.log(err))
+   return await saveCustomer({variables:{customerInput:customerInput}});
 }
-return {saveHandler,response}
+return {saveHandler}
 }
 export const useDeleteCustomer=(id:number)=>{
     const [deleteCustomer]=useMutation(DELETE_CUSTOMER);
