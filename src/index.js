ecf5f95456cbc8b84d80b858adfea12a4a675732
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-20 11:09:44 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const result='<405 Hello ,Mike,'
-console.log(result.substring(result.indexOf('<',result.indexOf(' '))))
-console.log(result.substring(result.indexOf('<')+1,result.indexOf(' ')))
+console.log(result.substring(result.indexOf('<')+1,result.indexOf(' ')));
+console.log(result.substring(result.indexOf('<'),result.indexOf(' ')));
