4b0a9c788c473b54ac4e53ee1bb5da74c9b090b0
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-21 12:02:35 +0200

    add report crud

--- src/pages/admin/components/navigationCrud/report/index.tsx
+++ src/pages/admin/components/navigationCrud/report/index.tsx
@@ -1,6 +1,76 @@
+import { styled } from '@mui/system';
+import { Tabs } from '@mui/base/Tabs';
+import { TabsList as BaseTabsList } from '@mui/base/TabsList';
+import { TabPanel as BaseTabPanel } from '@mui/base/TabPanel';
+import { buttonClasses } from '@mui/base/Button';
+import { Tab as BaseTab, tabClasses } from '@mui/base/Tab';
+import { blue } from '@mui/material/colors';
 export const ReportCrud=()=>{
     return(
         <>
+       <Tabs defaultValue={1}>
+      <TabsList className='mt-3'>
+        <Tab value={1}>One</Tab>
+        <Tab value={2}>Two</Tab>
+        <Tab value={3}>Three</Tab>
+      </TabsList>
+      <TabPanel value={1}>First page</TabPanel>
+      <TabPanel value={2}>Second page</TabPanel>
+      <TabPanel value={3}>Third page</TabPanel>
+    </Tabs>
         </>
     )
 }
+
+const Tab = styled(BaseTab)`
+  font-family: 'IBM Plex Sans', sans-serif;
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  line-height: 1.5;
+  padding: 8px 12px;
+  margin: 6px;
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  &:hover {
+    background-color: ${blue[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 3px solid ${blue[200]};
+  }
+
+  &.${tabClasses.selected} {
+    background-color: #fff;
+    color: ${blue[600]};
+  }
+
+  &.${buttonClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(BaseTabPanel)`
+  width: 100%;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+`;
+
+const TabsList = styled(BaseTabsList)(
+  ({ theme }) => `
+  min-width: 400px;
+  background-color: ${blue[500]};
+  border-radius: 12px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 4px 6px ${
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.4)' : 'rgba(0,0,0, 0.2)'
+  };
+  `,
+);
