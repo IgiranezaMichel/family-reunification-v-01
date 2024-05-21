a442fa0963d3a0c3bfd3e78c5a2a14f2cb56bc99
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-21 12:02:48 +0200

    add report crud

--- src/pages/admin/navigations/report.tsx
+++ src/pages/admin/navigations/report.tsx
@@ -1,8 +1,11 @@
+import { ReportCrud } from "../components/navigationCrud/report"
+
 export const Report=()=>{
     return(
         <>
         <div className="fs-4 fw-bold">
             Manage Report
+            <ReportCrud/>
         </div>
         </>
     )
