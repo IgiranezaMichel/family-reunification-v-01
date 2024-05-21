8235bb18da93b9a6941cdbdfc1d305cc277841c2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-21 18:04:59 +0200

    remove unused

--- src/pages/admin/components/navigationCrud/report/missedPeople.tsx
+++ src/pages/admin/components/navigationCrud/report/missedPeople.tsx
@@ -1,7 +1,7 @@
-import {  FilterBAndW } from "@material-ui/icons";
 import { Grid, Paper, styled } from "@mui/material"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
-
+import { countries } from "../../../../../object";
+import './index.css'
 export const MissedPeople = () => {
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -12,12 +12,32 @@ export const MissedPeople = () => {
     }));
     return (
         <main className="fw-normal">
-            <Grid>Recent Missed People</Grid>
-            <Grid2 spacing={1}>
-                <Grid >
-                    <Item><FilterBAndW/></Item>
-                </Grid>
+            <Grid2 container spacing={1} direction={'row'} border={0} overflow={'auto'}>
+                <Grid xs={4} border={1}>
+                    <Item elevation={0}>Filter by</Item>
+                 </Grid>
+                 <Grid xs={4} border={1}>
+                    <Item elevation={0}>
+                        <select name="" id="" style={{border:'none'}} className="border-0">
+                            <option value="">Location</option>
+                             {countries.map(data=><option>{data}</option>)}
+                        </select>
+                    </Item>
+                 </Grid>
+                 <Grid xs={4} border={1}>
+                    <Item elevation={0}>
+                        <small>From date</small>
+                        <input type="date" className="mx-1 border-0" />
+                    </Item>
+                 </Grid>
+                 <Grid xs={4} border={1}>
+                    <Item elevation={0}>
+                        <small>To date</small>
+                        <input type="date" className="mx-1 border-0" />
+                    </Item>
+                 </Grid>
             </Grid2>
+            
         </main>
     )
 }
