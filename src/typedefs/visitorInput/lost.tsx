b1963badea94c0ee43850083a2c2f05f1e274385
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-22 00:22:30 +0200

    add lost

---
+++ src/typedefs/visitorInput/lost.tsx
@@ -0,0 +1,18 @@
+export type LostDTO={
+    id: string
+    name: string
+    gender: string
+    address: string
+    phoneNumber: string
+    currentCountry: string
+    nativeCountry: string
+    base64Profile: string
+    dob: string
+    countryOfLost: string
+    expectedAddress: string
+    relationShip: string
+    hasFound: boolean
+    customerId: number
+    caseId: number
+    description: string
+}
