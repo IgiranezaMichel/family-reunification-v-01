b8f61d49f5bbcaa0d983a0bee4f5e1173663dfad
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-21 19:22:19 +0200

    add css

--- src/pages/admin/components/navigationCrud/report/index.tsx
+++ src/pages/admin/components/navigationCrud/report/index.tsx
@@ -1,76 +1,32 @@
-import { styled } from '@mui/system';
-import { Tabs } from '@mui/base/Tabs';
-import { TabsList as BaseTabsList } from '@mui/base/TabsList';
-import { TabPanel as BaseTabPanel } from '@mui/base/TabPanel';
-import { buttonClasses } from '@mui/base/Button';
-import { Tab as BaseTab, tabClasses } from '@mui/base/Tab';
-import { blue } from '@mui/material/colors';
-import { MissedPeople } from './missedPeople';
-export const ReportCrud=()=>{
+import { Ballot, Delete, Remove } from "@material-ui/icons"
+import { Card, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
+
+export const DisplayReport=()=>{
     return(
-        <>
-       <Tabs defaultValue={1}>
-      <TabsList className='mt-3' variant="scrollable">
-        <Tab value={1}>Recent  missed people report</Tab>
-        <Tab value={2}>Recent Found report</Tab>
-        <Tab value={3}>Recent Found report</Tab>
-      </TabsList>
-      <TabPanel value={1}><MissedPeople/></TabPanel>
-      <TabPanel value={2}>Second page</TabPanel>
-      <TabPanel value={3}>Third page</TabPanel>
-    </Tabs>
-        </>
+        <Card className="p-0 rounded-0">
+          <div ></div>
+          <Table> 
+            <TableHead className="border-1 bg-info">
+              <TableRow >
+                <TableCell>#</TableCell>
+                <TableCell><b>Name</b></TableCell>
+                <TableCell><b>Lost Date</b></TableCell>
+                <TableCell><b>Found Date</b></TableCell>
+                <TableCell className="text-center"><b>Action</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            <TableRow className="border-1">
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell className="text-center">
+                  <Ballot/> <Delete/>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
     )
 }
-
-const Tab = styled(BaseTab)`
-  font-family: 'IBM Plex Sans', sans-serif;
-  color: white;
-  cursor: pointer;
-  font-size: 0.875rem;
-  font-weight: bold;
-  background-color: transparent;
-  line-height: 1.5;
-  padding: 8px 12px;
-  margin: 6px;
-  border: none;
-  border-radius: 8px;
-  display: flex;
-  &:hover {
-    background-color: ${blue[400]};
-  }
-
-  &:focus {
-    color: #fff;
-    outline: 3px solid ${blue[200]};
-  }
-
-  &.${tabClasses.selected} {
-    background-color: #fff;
-    color: ${blue[600]};
-  }
-
-  &.${buttonClasses.disabled} {
-    opacity: 0.5;
-    cursor: not-allowed;
-  }
-`;
-
-const TabPanel = styled(BaseTabPanel)`
-  width: 100%;
-  font-family: 'IBM Plex Sans', sans-serif;
-`;
-
-const TabsList = styled(BaseTabsList)(
-  ({ theme }) => `
-  min-width: 400px;
-  background-color: ${blue[500]};
-  border-radius: 12px;
-  margin-bottom: 16px;
-  display: flex;
-  align-items: center;
-  box-shadow: 0px 4px 6px ${
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.4)' : 'rgba(0,0,0, 0.2)'
-  };
-  `,
-);
