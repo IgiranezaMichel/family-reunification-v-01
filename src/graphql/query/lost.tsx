0c07c893d3754643fcefc872582f55a659e7ec15
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-24 17:59:09 +0200

    add lost

--- src/graphql/query/lost.tsx
+++ src/graphql/query/lost.tsx
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const ALL_LOST=gql`
+export const ALL_LOST = gql`
 query{lostList{
   id
   name
@@ -20,6 +20,65 @@ query{lostList{
     description
   }
   dob
+  
+}}
+`
+export const CUSTOMER_POSTS = gql`
+query($customerId:ID){
+  customerLostPosts(customerId:$customerId){
+  id
+  name
+  profile
+  gender
+  address
+  phoneNumber
+  currentCountry
+  nativeCountry
+  hasFound
+  expectedAddress
+  timeStamp
+  document{
+    fileType
+    fileName
+    encodingName
+    file
+  }
+  cases{
+    title
+    description
+  }
+  
+}}
+`
+export const FIND_LOST_BY_ID = gql`
+query($customerId:ID){
+  findLostById(customerId:$customerId){
+  id
+  name
+  gender
+  address
+  phoneNumber
+  currentCountry
+  nativeCountry
+  profile
+  dob
+  countryOfLost
+  expectedAddress
+  relationShip
+  hasFound
+  timeStamp
+  document{
+    id
+    fileType
+    fileName
+    encodingName
+    file
+  }
+  cases{
+    title
+    description
+    timeStamp
+  }
   
 }}
 `
