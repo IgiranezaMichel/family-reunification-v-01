0e0dce1449bedf24b64db1ef311c987889b45068
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-25 18:46:51 +0200

    add customer posts

--- src/controller/lost/query.tsx
+++ src/controller/lost/query.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client"
-import { ALL_LOST } from "../../graphql/query/lost"
+import { ALL_LOST, CUSTOMER_POSTS, FIND_LOST_BY_ID } from "../../graphql/query/lost"
 import { useEffect, useState } from "react";
 import { Response } from "../../typedefs/response";
 
@@ -11,6 +11,43 @@ export const useLost=()=>{
             const fetch=async()=>{
                 if(data){
                     return data.lostList}
+            }
+            fetch().then(data=>{
+                setResponse({code:200,responseContent:data,responseReady:true});
+            })
+            .catch(err=>err)
+        },[data]
+    )
+    return {response,refetch}
+
+}
+export const useCustomerLostPost=(customerId:number)=>{
+    const [response,setResponse]=useState<Response>({code:0,responseContent:{},responseReady:false});
+    const {refetch,data}=useQuery(CUSTOMER_POSTS,{variables:{customerId:customerId}});
+    useEffect(
+        ()=>{
+            const fetch=async()=>{
+                if(data){
+                    return data.customerLostPosts}
+            }
+            fetch().then(data=>{
+                setResponse({code:200,responseContent:data,responseReady:true});
+            })
+            .catch(err=>err)
+        },[data]
+    )
+    return {response,refetch}
+
+}
+
+export const useFindLostPerson=(customerId:string)=>{
+    const [response,setResponse]=useState<Response>({code:0,responseContent:{},responseReady:false});
+    const {refetch,data}=useQuery(FIND_LOST_BY_ID,{variables:{customerId:customerId}});
+    useEffect(
+        ()=>{
+            const fetch=async()=>{
+                if(data){
+                    return data.findLostById}
             }
             fetch().then(data=>{
                 setResponse({code:200,responseContent:data,responseReady:true});
