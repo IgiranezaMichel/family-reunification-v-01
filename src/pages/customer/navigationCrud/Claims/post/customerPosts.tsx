2e17b318560605795af925f0c4b277e8f1e34106
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-26 18:12:01 +0200

    add json parser

--- src/pages/customer/navigationCrud/Claims/post/customerPosts.tsx
+++ src/pages/customer/navigationCrud/Claims/post/customerPosts.tsx
@@ -1,10 +1,19 @@
-import { Comment, Description, List } from "@material-ui/icons"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ArrowForward, Close } from "@material-ui/icons"
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator, timelineItemClasses } from "@mui/lab"
-import { Typography } from "@mui/material"
-export const CustomerPost=()=>{
-    return(
+import { Avatar, Button, Dialog, Tooltip, Typography } from "@mui/material"
+import { useCustomerLostPost } from "../../../../../controller/lost/query"
+import { LostDetail } from "./lostDetail"
+import { useState } from "react"
+export const CustomerPost = () => {
+    const user = JSON.parse(String(localStorage.getItem('user')));
+    const { response } = useCustomerLostPost(Number(user.id));
+    const [customerId,setCustomerId]=useState('');
+    const [showLostDetail,setShowLostDetail]=useState(false);
+    return (
         <>
-        <Timeline sx={{
+            {response.responseReady && response.responseContent != undefined &&
+                response.responseContent.length != 0 && response.responseContent.map((data: any) => <Timeline sx={{
                     [`& .${timelineItemClasses.root}:before`]: {
                         flex: 0,
                         padding: 0,
@@ -16,55 +25,30 @@ export const CustomerPost=()=>{
                             <TimelineConnector />
                         </TimelineSeparator>
                         <TimelineContent sx={{ py: '12px', px: 2 }}>
-                            <Typography variant="h6" component="span">
-                                Eat
+                            <Typography variant="h6" component="span" className="d-flex">
+                                <Tooltip arrow placement="top" title={<><img className="col-md-6 card-img" src={data.profile} alt="" /></>}><Avatar src={data.profile} /></Tooltip> <div className="card d-flex justify-content-center align-items-center bg-transparent border-0 mx-2"> {data.name}</div>
+                                <section>{data.description}</section>
                             </Typography>
-                            <Typography>Because you need strength</Typography>
                             <Typography>
-                                <small>on 23/07/2023</small>
+                                <b>status </b> {data.hasFound ? <span className="text-success">Has Found</span> : <span>Not Found</span>}
                             </Typography>
-                            <div className="modal-footer border-0">
-                                <Description /> <Comment /> <List />
+                            <Typography>
+                                <b>Expected Address </b> {data.expectedAddress}
+                            </Typography>
+                            <Typography className="float-end">
+                                <small >{data.timeStamp}</small>
+                            </Typography>
+                            <div style={{ clear: 'both' }} className="modal-footer border-0">
+                                <Button onClick={()=>{setCustomerId(data.id);setShowLostDetail(true)}}><ArrowForward />  </Button>
                             </div>
                         </TimelineContent>
                     </TimelineItem>
-                    <TimelineItem>
-                        <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent sx={{ py: '12px', px: 2 }}>
-                            <Typography variant="h6" component="span">
-                                Eat
-                            </Typography>
-                            <Typography>Because you need strength</Typography>
-                            <Typography>
-                                <small>on 23/07/2023</small>
-                            </Typography>
-                            <div className="modal-footer border-0">
-                                <Description /> <Comment /> <List />
-                            </div>
-                        </TimelineContent>
-                    </TimelineItem>
-                    <TimelineItem>
-                        <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
-                        </TimelineSeparator>
-                        <TimelineContent sx={{ py: '12px', px: 2 }}>
-                            <Typography variant="h6" component="span">
-                                Eat
-                            </Typography>
-                            <Typography>Because you need strength</Typography>
-                            <Typography>
-                                <small>on 23/07/2023</small>
-                            </Typography>
-                            <div className="modal-footer border-0">
-                                <Description /> <Comment /> <List />
-                            </div>
-                        </TimelineContent>
-                    </TimelineItem>
-                </Timeline>
+                </Timeline>)}
+                <Dialog open={showLostDetail} maxWidth='md' PaperProps={{className:'col-12 rounded-0'}}>
+                    <LostDetail customerId={customerId}>
+                    <div className="p-3 fw-bold sticky-top bg-white">Details <Close onClick={()=>setShowLostDetail(false)} className="float-end"/></div>
+                    </LostDetail>
+                </Dialog>
         </>
     )
 }
