79ef45f951321b33cf3710e184c949e8351b57ac
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-26 18:12:41 +0200

    add lost page input

---
+++ src/typedefs/lostPage.tsx
@@ -0,0 +1,9 @@
+export type  LostPageInput= {
+    hasFound: boolean
+    country: string
+    fromDate: string
+    toDate: string
+    pageNumber: number
+    pageSize: number
+    sort: string
+  }
