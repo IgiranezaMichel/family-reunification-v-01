5e9934c24544cbdfa9b392e3f3cc2912abd9a6a1
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-26 18:11:38 +0200

    add avatar

--- src/pages/customer/navigationCrud/Claims/post/allPosts.tsx
+++ src/pages/customer/navigationCrud/Claims/post/allPosts.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ArrowForwardRounded, LocationOn, MessageTwoTone, Person } from "@material-ui/icons"
-import { Box, Button, Grid, Paper, styled } from "@mui/material"
+import { ArrowForwardRounded, Close, LocationOn, MessageTwoTone, Person } from "@material-ui/icons"
+import { Avatar, Box, Button, Dialog, Grid, Paper, styled } from "@mui/material"
 import { useLost } from "../../../../../controller/lost/query"
+import { useState } from "react";
+import { LostSummary } from "./lostSummary";
 
 export const AllPost = () => {
     const { response } = useLost();
@@ -12,7 +14,8 @@ export const AllPost = () => {
         textAlign: 'justify',
         color: theme.palette.text.secondary,
     }));
-    console.log(response)
+    const [customerId,setCustomerId]=useState('');
+    const [showLostDetail,setShowLostDetail]=useState(false);
     return (
         <>
             <Grid container spacing={2} direction={'row'} p={0}>
@@ -22,15 +25,21 @@ export const AllPost = () => {
                             <Item sx={{ p: 0 }} className="card ">
                                 <img style={{ borderBottomRightRadius: '50%', objectFit: 'cover', borderBottomLeftRadius: '50%', overflow: 'hidden', height: '30dvh' }} className="col-12" src={data.profile} />
                                 <div className="card-img-overlay p-0">
-                                    <small>Country:{data.currentCountry}</small>
+                                    <section className="d-flex" style={{backgroundColor:'rgb(255,255,255,.8)'}}>
+                                    <Avatar src={data.postedBy.profilePicture}/>
+                                    <div className="card d-flex justify-content-center fw-bold bg-transparent border-0 align-content-center">
+                                        {data.name}
+                                    </div>
+                                    </section>
                                 </div>
                                 <p><Person />{data.name}</p>
                                 <p><LocationOn />{data.address}</p>
+                                <p className="modal-footer mx-2"><small >{data.currentCountry}</small></p>
                                 <Box className="d-flex justify-content-between">
                                     <Button>
                                         <MessageTwoTone />
                                     </Button>
-                                    <Button>
+                                    <Button onClick={()=>{setCustomerId(data.id);setShowLostDetail(true)}} sx={{':hover':{transition:'all 0.4s ease-out',scale:'1.2'}}}>
                                         <ArrowForwardRounded />
                                     </Button>
                                 </Box>
@@ -38,6 +47,11 @@ export const AllPost = () => {
                         </Grid>
                     })}
             </Grid>
+            <Dialog open={showLostDetail} maxWidth='md' PaperProps={{className:'col-12 rounded-0'}}>
+                    <LostSummary customerId={customerId}>
+                    <div className="p-3 fw-bold sticky-top bg-white">Details <Close onClick={()=>setShowLostDetail(false)} className="float-end"/></div>
+                    </LostSummary>
+                </Dialog>
         </>
     )
 }
